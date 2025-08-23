cb23b0f82ae71900e6557490123834368dadfac5
Oliver Schafeld <oliver@schafeld.com>
2025-08-23 11:09:27 +0200

    archive simplest custom component version

---
+++ src/storyblok/OllisNewUniversalBlock_v1.tsx
@@ -0,0 +1,32 @@
+// Keeping this file for reference
+// This is the simplest possible version of the custom component
+
+import React from 'react';
+import { SbBlokData } from '@storyblok/react/rsc';
+
+
+import type { AssetContent } from '../delivery-api';
+
+interface OllisNewUniversalBlockProps {
+  blok: SbBlokData & {
+    text?: string;
+    image?: AssetContent;
+  };
+}
+
+const OllisNewUniversalBlock: React.FC<OllisNewUniversalBlockProps> = ({ blok }) => {
+  return (
+    <div className="ollis-new-universal-block">
+      {blok.text && <p>{blok.text}</p>}
+      {blok.image?.filename && (
+        <img
+          src={blok.image.filename}
+          alt={blok.image.alt || 'Image'}
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default OllisNewUniversalBlock;
